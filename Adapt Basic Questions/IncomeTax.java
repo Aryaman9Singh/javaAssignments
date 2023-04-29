6cd4b0a3c73bea8623efe2acf7d878d6d90df73a
Aryaman9Singh <aryamanpuneetsingh@gmail.com>
2023-04-30 01:13:25 +0530

    second commit

---
+++ Adapt Basic Questions/IncomeTax.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class IncomeTax {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter CTC: ");
+		int ctc = sc.nextInt();
+		
+		if(ctc>=0 && ctc<=180000)
+			System.out.print("Tax payable: 0");
+		if(ctc>=181000 && ctc<=300000)
+			System.out.print("Tax payable: " + ctc*0.1);
+		if(ctc>=300001 && ctc<=500000)
+			System.out.print("Tax payable: " + ctc*0.2);
+		if(ctc>=500001 && ctc<=1000000)
+			System.out.print("Tax payable: " + ctc*0.3);
+			
+	}    
+}
---
+++ Adapt Basic Questions/StudentResult.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class StudentResult {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter marks of subject 1: ");
+		int a = sc.nextInt();
+		System.out.print("Enter marks of subject 2: ");
+		int b = sc.nextInt();
+		System.out.print("Enter marks of subject 3: ");
+		int c = sc.nextInt();
+		
+		if(a>60 && b>60 && c>60)
+			System.out.print("Passed");
+		else if(a<=60 && b<=60 || b<=60 && c<=60 || a<=60 && c<=60)
+			System.out.print("Failed");
+		else
+			System.out.print("Promoted");
+	}
+
+}
